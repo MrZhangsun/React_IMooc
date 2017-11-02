7ef5033ed55c8cda7073b6c18fb8ad4d8648e102
zhangsunjiankun <zhangsunjiankun@bldz.com>
2017-11-02 14:49:12 +0800

    添加PC和App端页脚

---
+++ src/js/components/mobile_header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Row,Col} from "antd";
+import 'antd/dist/antd.css';
+
+export default class MobileHeader extends React.Component{
+    render(){
+        return(
+            <div id={"mobileHeader"}>
+                <header>
+                    <Row>
+                        <Col span={2}></Col>
+                        <Col span={4}>
+                            <a href='/' className='logo'>
+                                <img src='./src/images/logo.png' alt='logo'/>
+                                <span>ReactNews</span>
+                            </a>
+                        </Col>
+                        <Col span={2}></Col>
+                    </Row>
+                </header>
+            </div>
+        );
+    };
+}
